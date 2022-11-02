c76912df3fe09096c77456b4a3111b2bb3775aff
Iago Sanjurjo <Iago Sanjurjo>
2022-11-02 20:21:56 +0100

    Before video player

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,11 +1,43 @@
+<script setup lang="ts">
+const course = useCourse()
+const route = useRoute()
+
+console.log(route.params.chapterSlug, route.params.lessonSlug, course)
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  )
+})
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  )
+})
+</script>
 <template>
-  <article class="lesson">
-    <h2>Lecciones</h2>
-    <p>Esto es una lección</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <article class="lessons">
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson.sourceUrl"
+        :href="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+      >
+        Download Source Code
+      </a>
+      <a
+        v-if="lesson.downloadUrl"
+        :href="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+      >
+        Download Video</a
+      >
+    </div>
+    <p>{{ lesson.text }}</p>
   </article>
 </template>
-<script setup lang="ts"></script>
 
 <style scoped></style>
